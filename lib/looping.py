f01a25c779e81bf99c6f5383089563ff5ce0a56c
BensonGitau <bensonkamaugitau@gmail.com>
2024-05-20 15:57:11 +0300

    complete lab

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,39 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    i = 10
+    while i > 0:
+        print(i)
+        i -= 1
+    print('Happy New Year!')
+happy_new_year()
+
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    squired_ints = [int*int for int in int_list]
+    return squired_ints
+
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    i = 0
+    while i < 100:
+        i +=1
+        if i%3 == 0 and i%5 ==0:
+            print('FizzBuzz')
+        elif i%3 == 0:
+            print('Fizz')
+        elif i%5 == 0:
+            print('Buzz')
+        else:
+            print(i)
+fizzbuzz()
+
+
+for i in range(10):
+    print("Looping!")
+    print(f"i is: {i}")
+
+    
+player_heights = [0.008, 0.008, 0.008, 0.009, 0.008, 0.01, 0.009, 0.009, 0.01, 0.008, 0.009, 0.009, 0.008, 0.008, 0.008, 0.009, 0.008, 0.009, 0.01, 0.01]
+player_heights = [height * 7920 for height in player_heights]
+print(player_heights)
